90d4438351faa07e90fa3c0b5052bf0c4c44b49d
joseph-zabaleta <joseph.l.zabaleta@gmail.com>
2020-08-23 19:12:25 -0700

    list feature completed

--- index.js
+++ index.js
@@ -13,7 +13,8 @@ program
     .option('-i, --import <path>', 'imports a CSV file from provided path, if --cname is not present the file name will be used')
     .option('--cname <name>', 'Collection name: Optional secondary flag for file name')
     .option('-p, --pairs', 'random pairs')
-    .option('-d, --default [new set]', 'View current active set or if a value is provided, update the current active set to the value provided.');
+    .option('-d, --default [new set]', 'View current active set or if a value is provided, update the current active set to the value provided.')
+    .option('-l, --list', 'List all available/imported collections');
 
 
 let userInput = program.parse(process.argv);
@@ -22,7 +23,7 @@ let userInput = program.parse(process.argv);
 if (userInput.import) {
     fileImport(userInput.import, userInput.cname);
 
-} else if (userInput.pairs === true) {
+} else if (userInput.pairs) {
 
     pairs();
 
@@ -34,6 +35,12 @@ if (userInput.import) {
     };
 
     console.log(`Current set: ${settings.getDefault()}`);
+} else if (userInput.list) {
+
+    console.log(`Current set: ${settings.getDefault()}\n`);
+    console.log('Available Collections:');
+    console.log(settings.getCollections());
+
 }
 
 
