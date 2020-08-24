cf1070995b601f0805d287c858199f12189ccc41
joseph-zabaleta <joseph.l.zabaleta@gmail.com>
2020-08-23 18:53:43 -0700

    Added --set options

--- index.js
+++ index.js
@@ -1,15 +1,19 @@
 'use strict';
 
+/** 3rd Party Libraries */
 const { program } = require('commander');
 
+/** Local  */
 const pairs = require('./lib/pairs/pair-logic.js');
 const fileImport = require('./lib/import/import-logic.js');
+const settings = require('./lib/settings/settings.js');
 
 program
     .description('An application to create random groups')
     .option('-i, --import <path>', 'imports a CSV file from provided path, if --cname is not present the file name will be used')
     .option('--cname <name>', 'Collection name: Optional secondary flag for file name')
-    .option('-p, --pairs', 'random pairs');
+    .option('-p, --pairs', 'random pairs')
+    .option('-d, --default [new set]', 'View current active set or if a value is provided, update the current active set to the value provided.');
 
 
 let userInput = program.parse(process.argv);
@@ -22,6 +26,17 @@ if (userInput.import) {
 
     pairs();
 
-} else {
+} else if (userInput.default) {
+
+    if (userInput.default !== true) {
+        settings.setDefault(userInput.default);
+        settings.save();
+    };
+
+    console.log(`Current set: ${settings.getDefault()}`);
+}
+
+
+else {
     throw new Error('Command not recognized');
 };
